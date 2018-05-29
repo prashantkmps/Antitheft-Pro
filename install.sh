f125d57dcddd187bc6fd908cc1f8e6f25405a56d
prateek <prateekagrawal89760@gmail.com>
2018-05-29 08:33:28 +0530

    Commited from Pycharm

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sudo apt-get install python-pip
 sudo apt-get install python-opencv
 sudo apt-get install build-essensials
 
-sudo pip install -s requirements.txt
+sudo pip install -r requirements.txt
